c144ac8fceecbffda572a1967cf0d7ec8c029ace
Plip10101 <gamejamplipplop@gmail.com>
2020-11-14 11:07:57 +0530

    Added an 'edgebox' to detect when balloon goes to edge

---
+++ Assets/Scripts/Edgebox.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Edgebox : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        
+        if (collision.gameObject.GetComponent<Balloon>())
+            collision.gameObject.GetComponent<Balloon>().onedge = false;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+      
+        if(collision.gameObject.GetComponent<Balloon>())
+        collision.gameObject.GetComponent<Balloon>().onedge = true;
+    }
+}
