5dbb9a5077d192da68dffefe08f9bf822610088d
samYemmY <samuelfemboeck@gmail.com>
2020-11-14 09:19:23 +0100

    fix

--- Assets/Scripts/Stone.cs
+++ Assets/Scripts/Stone.cs
@@ -10,7 +10,7 @@ public class Stone : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+        
     }
 
     // Update is called once per frame
