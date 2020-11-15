88aca507b73135c2a80e301622489800f43551c0
Plip10101 <gamejamplipplop@gmail.com>
2020-11-15 15:48:37 +0530

    Player can no longer move diagonally

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -23,21 +23,32 @@ public class PlayerMovement : MonoBehaviour
 
         currentx = Input.GetAxisRaw("Horizontal");
         currenty = Input.GetAxisRaw("Vertical");
+        bool onlyone = (currenty == 0 || currentx == 0);
+        if (onlyone)
+        { 
         animator.SetFloat("currentx", currentx);
         animator.SetFloat("currenty", currenty);
+
+        Vector3 move = new Vector3(currentx * Speed * Time.deltaTime, currenty * Speed * Time.deltaTime);
+
+
         Bounds spriteBounds = GetComponent<SpriteRenderer>().bounds;
-        Vector3 move = new Vector3( currentx* Speed * Time.deltaTime, currenty * Speed * Time.deltaTime);
         Vector3 newPos = transform.position + move;
         Bounds newBounds = new Bounds(newPos, spriteBounds.size);
         if (Map.Instance.Encompasses(newBounds))
             transform.Translate(move);
-        if (move.sqrMagnitude > 0)
-        {
-            lastx = currentx;
-            lasty = currenty;
-            animator.SetFloat("lastx", lastx);
-            animator.SetFloat("lasty", lasty);
-            animator.SetBool("walk", true);
+            
+            if (move.sqrMagnitude > 0)
+            {
+                lastx = currentx;
+                lasty = currenty;
+                animator.SetFloat("lastx", lastx);
+                animator.SetFloat("lasty", lasty);
+                animator.SetBool("walk", true);
+            }
+            else animator.SetBool("walk", false);
+
+
         }
         else animator.SetBool("walk", false);
     }
