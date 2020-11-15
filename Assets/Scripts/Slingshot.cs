d50ea7d73896e7659981bd9a479878f67cf07ea3
Plip10101 <gamejamplipplop@gmail.com>
2020-11-15 16:28:38 +0530

    Added right and left animations for slingshot

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -7,27 +7,36 @@ public class Slingshot : MonoBehaviour
     public GameObject StonePrefab;
     public GameEvent OnSlingshot;
     Vector3 _initialMousePos;
-
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
+        {
             _initialMousePos = Input.mousePosition;
-
+            animator.SetTrigger("charge");
+        }
         if (Input.GetMouseButtonUp(0))
         {
             if (_initialMousePos != Input.mousePosition)
             {
-                OnSlingshot.Raise();
-                GameObject stone = Instantiate(StonePrefab, transform.position, Quaternion.identity);
-                stone.GetComponent<Stone>().Direction = (_initialMousePos - Input.mousePosition).normalized;
+                animator.SetTrigger("shoot");
+               
             }
+            else animator.SetTrigger("default");
         }
+    }
+
+    public void createrock()
+    {
+        OnSlingshot.Raise();
+        GameObject stone = Instantiate(StonePrefab, transform.position, Quaternion.identity);
+        stone.GetComponent<Stone>().Direction = (_initialMousePos - Input.mousePosition).normalized;
     }
 }
